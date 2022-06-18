029d5e06219776862e9cbfc3f922cff8a6213c94
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-06-18 09:34:12 -0400

    Add Arma server commands

--- src/berconpy/ext/arma/client.py
+++ src/berconpy/ext/arma/client.py
@@ -7,3 +7,54 @@ log = logging.getLogger(__name__)
 
 class AsyncArmaRCONClient(AsyncRCONClient):
     """An AsyncRCONClient subclass that adds more methods for handling Arma RCON."""
+
+    async def lock_server(self) -> str:
+        """Locks the server and prevents new players from joining."""
+        return await self.send_command('#lock')
+
+    async def restart_and_reassign(self) -> str:
+        """Restarts the mission and reassigns player roles."""
+        return await self.send_command('#reassign')
+
+    async def restart_mission(self) -> str:
+        """Restarts the currently running mission."""
+        return await self.send_command('#restart')
+
+    async def restart_server(self) -> str:
+        """Tells the server to restart.
+
+        Note that this client will not close automatically, rather
+        the connect() context manager should be exited or close()
+        should be called. Otherwise, the client will continue to
+        attempt reconnecting to the server.
+
+        """
+        return await self.send_command('#restartserver')
+
+    async def select_mission(self, mission: str, difficulty: str = '') -> str:
+        """Selects a new mission for the server to load.
+
+        :param mission: The name of the mission to load
+            without the file extension (e.g. 'MP_Bootcamp_01.Altis').
+        :param difficulty:
+            The new difficulty to use on the server (e.g. Recruit,
+            Regular, Veteran, Custom). If not provided, the current
+            difficulty is reused.
+
+        """
+        return await self.send_command(f'#mission {mission} {difficulty}'.rstrip())
+
+    async def shutdown_server(self) -> str:
+        """Tells the server to shut down.
+
+        Note that this client will not close automatically, rather
+        the connect() context manager should be exited or close()
+        should be called. Otherwise, the client will continue to
+        attempt reconnecting to the server.
+
+        """
+        return await self.send_command('#shutdown')
+
+    async def unlock_server(self) -> str:
+        """Unlocks the server and allows new players to join."""
+        return await self.send_command('#unlock')
