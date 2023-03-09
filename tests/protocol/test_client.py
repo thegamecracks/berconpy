8916de806d611a5cc994279f95c24bd0679be256
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-09 13:39:02 -0500

    Add test for client command validation

--- tests/protocol/test_client.py
+++ tests/protocol/test_client.py
@@ -70,3 +70,33 @@ def test_invalid_states(client: RCONClientProtocol):
         client.invalidate_command(0)
 
         client.reset()
+
+
+def test_command_validation(client: RCONClientProtocol):
+    client.receive_datagram(ServerLoginPacket(success=True).data)
+    client.events_received()
+
+    # Unknown command
+    packet = ServerCommandPacket(sequence=0, total=1, index=0, response=b"")
+    with pytest.raises(ValueError):
+        client.receive_datagram(packet.data)
+
+    seq = client.send_command("").sequence
+
+    # Allow out-of-order packets
+    packet = ServerCommandPacket(sequence=seq, total=2, index=1, response=b"world!")
+    client.receive_datagram(packet.data)
+
+    # Mismatched total
+    packet = ServerCommandPacket(sequence=seq, total=1, index=0, response=b"")
+    with pytest.raises(ValueError):
+        client.receive_datagram(packet.data)
+
+    # Repeat packet
+    packet = ServerCommandPacket(sequence=seq, total=2, index=1, response=b"")
+    with pytest.raises(ValueError):
+        client.receive_datagram(packet.data)
+
+    packet = ServerCommandPacket(sequence=seq, total=2, index=0, response=b"Hello ")
+    client.receive_datagram(packet.data)
+    assert first_and_only_event(client, ClientCommandEvent).message == "Hello world!"
