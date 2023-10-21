c5910b837905a7d19f92f1e77644d1499e74ed06
Rémi Gaffet <remigft@gmail.com>
2023-10-21 12:16:55 +0200

    fix: integration of pull request suggestions

--- examples/kick_survivor.py
+++ examples/kick_survivor.py
@@ -1,4 +1,4 @@
-"""Connects to an RCON server and prevents players with the nickname 'Survivor' from logging in"""
+"""Connects to an RCON server and kicks players with "Survivor" as their name."""
 import re, math
 import asyncio
 
@@ -8,21 +8,26 @@ IP_ADDR = "XXX.XXX.XXX.XXX"
 PORT = 9999
 PASSWORD = "ASCII_PASSWORD"
 
-kickReason = "Name 'Survivor' not allowed"
 client = rcon.AsyncRCONClient()
+
 
 @client.dispatch.on_message
 async def on_message(message: str):
-    match = re.match(r'Player #(\d+) (\w+(?: \(\d+\))?) \(\d+\.\d+\.\d+\.\d+:\d+\) connected', message)
+    match = re.match(
+        r"Player #(\d+) (\w+(?: \(\d+\))?) \(\d+\.\d+\.\d+\.\d+:\d+\) connected",
+        message,
+    )
     if match:
         player_id = match.group(1)
         player_name = match.group(2)
-        if player_name == "Survivor" or re.match(r'^Survivor \(\d+\)$', player_name):
-            await client.kick(int(player_id), kickReason)
+        if player_name == "Survivor" or re.match(r"^Survivor \(\d+\)$", player_name):
+            await client.kick(int(player_id), "Name 'Survivor' not allowed")
+
 
 async def main():
     async with client.connect(IP_ADDR, PORT, PASSWORD):
         await asyncio.sleep(math.inf)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
