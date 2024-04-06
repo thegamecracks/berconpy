4d820efe7118e505bc34f3fcb60652449ef7af82
Draowyr <remigft@gmail.com>
2024-04-06 03:58:00 +0200

    feat: add kick_survivor.py example (#6)
    Co-authored-by: thegamecracks <61257169+thegamecracks@users.noreply.github.com>

---
+++ examples/kick_survivor.py
@@ -0,0 +1,27 @@
+"""Connects to an RCON server and kicks players with "Survivor" as their name."""
+import asyncio
+import math
+import re
+
+import berconpy as rcon
+
+IP_ADDR = "XXX.XXX.XXX.XXX"
+PORT = 9999
+PASSWORD = "ASCII_PASSWORD"
+
+client = rcon.AsyncRCONClient()
+
+
+@client.dispatch.on_player_connect
+async def on_player_connect(player: rcon.Player):
+    if re.match(r"Survivor(?: \(\d+\))?", player.name) is not None:
+        await player.kick("Name 'Survivor' not allowed")
+
+
+async def main():
+    async with client.connect(IP_ADDR, PORT, PASSWORD):
+        await asyncio.sleep(math.inf)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
